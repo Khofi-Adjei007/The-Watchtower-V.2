60d1777717f481f670577d678d2342ae1337097f
Khofi-Adjei007 <khofiadjei@gmail.com>
2024-04-15 02:02:46 +0000

    Officer Account Home Page : (Built the nav bar section and lead information bar,next is main body

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // Other configuration options...
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+    corePlugins: {
+      // Enable fractional widths
+      width: ['responsive', 'hover', 'focus', 'group-hover'],
+    },
+  }
+  
